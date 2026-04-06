307f339594908595283ba7fa0c4a422adfbc9d26
moniexe <moni.exe1@gmail.com>
2026-04-06 22:03:09 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,32 @@
-abstract class Room {
-    protected int numberOfBeds;
-    protected double size;
-    protected double price;
 
-    public Room(int numberOfBeds, double size, double price) {
-        this.numberOfBeds = numberOfBeds;
-        this.size = size;
-        this.price = price;
+import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
     }
 
-    public void displayDetails() {
-        System.out.println("Beds: " + numberOfBeds);
-        System.out.println("Size: " + size + " sq.ft");
-        System.out.println("Price: ₹" + price);
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
     }
 
-    public abstract String getRoomType();
-}
-
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super(1, 120.0, 2000.0);
+    public void updateAvailability(String roomType, int count) {
+        if (inventory.containsKey(roomType)) {
+            inventory.put(roomType, count);
+        }
     }
 
-    public String getRoomType() {
-        return "Single Room";
-    }
-}
-
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super(2, 200.0, 3500.0);
-    }
-
-    public String getRoomType() {
-        return "Double Room";
-    }
-}
-
-class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super(3, 350.0, 7000.0);
-    }
-
-    public String getRoomType() {
-        return "Suite Room";
+    public void displayInventory() {
+        System.out.println("=== Room Inventory ===\n");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " -> Available: " + entry.getValue());
+        }
     }
 }
 
@@ -53,28 +35,19 @@ public class Main {
 
         System.out.println("Welcome to Book My Stay App\n");
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        RoomInventory inventory = new RoomInventory();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        inventory.displayInventory();
 
-        System.out.println("=== Room Details ===\n");
+        System.out.println("\nChecking availability for Single Room:");
+        System.out.println("Available: " + inventory.getAvailability("Single Room"));
 
-        System.out.println(single.getRoomType());
-        single.displayDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
+        System.out.println("\nUpdating availability for Single Room...");
+        inventory.updateAvailability("Single Room", 4);
 
-        System.out.println(doubleRoom.getRoomType());
-        doubleRoom.displayDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
+        System.out.println("\nUpdated Inventory:\n");
+        inventory.displayInventory();
 
-        System.out.println(suite.getRoomType());
-        suite.displayDetails();
-        System.out.println("Available: " + suiteAvailable + "\n");
-
-        System.out.println("Thank you for using Book My Stay App!");
+        System.out.println("\nThank you for using Book My Stay App!");
     }
 }
